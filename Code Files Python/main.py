9a8d8f393d9a828603f63fe4642aaecaea9ba647
Ameer Hamza <18100216@lums.edu.pk>
2019-10-31 16:25:35 -0400

    Removed unnecessary print statements

--- Code Files Python/main.py
+++ Code Files Python/main.py
@@ -17,12 +17,13 @@ numberOfMultipleRuns = 3;  # Number of multiple runs to find average
 """ CLASSES """
 
 class Data():
-	"""docstring for Data"""
+	"""Data class to store training, testing and unlabeled data for two datasets"""
 	def __init__(self, typeOfDataset, typeOfSampling):
 		self.datasetName = typeOfDataset;
 		self.samplingType = typeOfSampling;
 		self.data = dict();
 
+	# load the data
 	def loadData(self):
 		labels = ["testing", "training", "unlabeled"];
 		typesOfDS = ["Matrix", "Labels"];
@@ -50,8 +51,9 @@ class Data():
 
 					self.data[label][typeOfDS].append(listt);
 
+
 class Results():
-	"""docstring for Results"""
+	"""Results class to store the accuracy results for two datasets"""
 	def __init__(self, typeOfDataset, typeOfSampling):
 		super(Results, self).__init__()
 		self.datasetName = typeOfDataset;
@@ -59,11 +61,13 @@ class Results():
 		self.samplingType = typeOfSampling;
 		self.averageAccuracies = [];
 		
+	# store the given accuracy for a specific runNumber
 	def storeAccuracy(self, accuracy, runNumber):
 		if runNumber not in self.accuracies:
 			self.accuracies[runNumber] = [];
 		self.accuracies[runNumber].append(accuracy);
 
+	# calculate average of accuracies for different runs of algorithm
 	def calculateAvgAccuracy(self):
 		for x in range(N):
 			self.averageAccuracies.append(statistics.mean([self.accuracies[0][x], 
@@ -72,6 +76,7 @@ class Results():
 
 """ FUNCTIONS """
 
+# active learning algorithm for multiple runs to calculate mean
 def activeLearningMultiple(Data, Results):
 
 	for runNumber in range(numberOfMultipleRuns):
@@ -80,6 +85,7 @@ def activeLearningMultiple(Data, Results):
 	Results.calculateAvgAccuracy();
 
 
+# active learning algorithm for a single run
 def activeLearning(Data, Results, runNumber):
 	
 	for numIter in range(N):
@@ -99,6 +105,7 @@ def activeLearning(Data, Results, runNumber):
 		# calculate the probabilities of all unlabeled samples being in each class
 		probsUnlabeledData = lrTrainedModel.predict_proba(Data.data['unlabeled']['Matrix'][runNumber]);
 
+		# check the type of sampling
 		if Data.samplingType == 'random':
 			kValuesSelected = randomSampling(probsUnlabeledData);
 		else:	
@@ -107,45 +114,73 @@ def activeLearning(Data, Results, runNumber):
 		kValuesSelected = sorted(kValuesSelected, reverse=True);
 
 		for x in kValuesSelected:
+			# adjust samples -> delete some samples from unlabeled data, add to training data
 			adjustSamples(Data, x, runNumber);
 	
 
+# adjust samples
 def adjustSamples(Data, sampleNum, runNumber):
 
 	toDelete = Data.data['unlabeled']['Matrix'][runNumber][sampleNum];
+	
+	# add some samples from unlabeled data to training data
 	Data.data['training']['Matrix'][runNumber] = np.append(Data.data['training']['Matrix'][runNumber], [toDelete], axis=0);
+	
+	# delete those samples from unlabeled data
 	Data.data['unlabeled']['Matrix'][runNumber] = np.delete(Data.data['unlabeled']['Matrix'][runNumber], sampleNum, axis=0);
 
 	toDelete = Data.data['unlabeled']['Labels'][runNumber][sampleNum];
+	
+	# add corresponding labels for samples added from unlabeled data to training data
 	Data.data['training']['Labels'][runNumber] = np.append(Data.data['training']['Labels'][runNumber], [toDelete]);
+	
+	# delete those labels from unlabeled data
 	Data.data['unlabeled']['Labels'][runNumber] = np.delete(Data.data['unlabeled']['Labels'][runNumber], sampleNum);
 
 
+# random sampling of 'k' samples from unlabeled data
 def randomSampling(probabilityData):
+
 	numSamples = len(probabilityData)
 	return [random.randint(0, numSamples-1) for x in range(k)];
 
+
+# uncertainty based sampling of 'k' samples from unlabeled data 
 def uncertaintyBasedSampling(probabilityData):
+	
 	d = dict();
 	numClasses = len(probabilityData[0]);
+	
+	# calculate classification entropy for each sample
 	for x in range(len(probabilityData)):
 		sum = 0;
 		for y in range(numClasses):
 			sum += probabilityData[x][y]*(math.log(probabilityData[x][y], 2));
 		sum *= -1;
 		d[sum] = x;
+	
 	x = 0;
 	topKValues = [];
+	
+	# select 'k' samples with highest entropy
 	for key in sorted(d, reverse=True):
 		topKValues.append(d[key]);
 		x += 1;
 		if x == k:
 			return topKValues;
 
+# draw a graph for random and uncertainty based sampling for given dataset
 def drawGraph(resultsRandom, resultsUncertaintyBased):
+	
 	iterations = range(1, N+1);
+	
+	# random sampling 
 	plt.plot(iterations, resultsRandom.averageAccuracies, label='accuracy for random sampling');
+	
+	# uncertainty based sampling
 	plt.plot(iterations, resultsUncertaintyBased.averageAccuracies, label='accuracy for uncertainty based sampling');
+	
+	# setting plot data and showing it
 	plt.xlabel('Iterations');
 	plt.ylabel('Accuracy');
 	plt.title('Accuracy on ' + resultsUncertaintyBased.datasetName + ' dataset');
